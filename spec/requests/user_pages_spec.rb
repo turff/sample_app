5abedd6c73ccb0e647a85eebafe416b28d94c2c8
Kevin McInturff <turff87@hotmail.com>
2012-05-07 16:37:41 -0400

    Adding additional exercises from chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,6 +14,21 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "error messages" do
+      	before {click_button submit}
+
+      	it { should have_selector('title', text: 'Sign up') } 
+      	it { should have_content('error') }
+      	it { should have_content("Password can't") }
+      	it { should have_content("Name can't") }
+      	it { should have_content("Email can't") }
+      	it { should have_content("Email is invalid") }
+      	it { should have_content("too short") }
+      	it { should have_content("Password confirmation") }
+
+      end
+
     end
 
     describe "with valid information" do
@@ -26,6 +41,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+      	before { click_button submit }
+      	let(:user) { User.find_by_email('user@example.com') }
+
+      	it { should have_selector('title', text: user.name) }
+      	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
