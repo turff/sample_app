0b615f1cd96b3e19a90d86b6ff9a1f7cc8dbd94a
Kevin McInturff <turff87@hotmail.com>
2012-05-07 15:34:54 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
-  # get "static_pages/home"
-  # get "static_pages/help"
-  # get "static_pages/about"
-  # get "static_pages/contact"
   root to: 'static_pages#home'
 
   match '/help', to: 'static_pages#help'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name			"Beezus Quinby"
+		email			"ramonasSis@fiction.com"
+		password	"foobar"
+		password_confirmation "foobar"
+	end
+end
